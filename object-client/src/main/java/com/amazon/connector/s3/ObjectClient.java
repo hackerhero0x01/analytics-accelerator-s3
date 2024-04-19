9b14006cc1bce0cbed8d846e4d55e469e54b53db
Ahmar Suhail <ahmarsu@amazon.co.uk>
2024-04-19 10:56:58 +0100

    Adds in head and get object operations for object client.

--- object-client/src/main/java/com/amazon/connector/s3/ObjectClient.java
+++ object-client/src/main/java/com/amazon/connector/s3/ObjectClient.java
@@ -1,4 +1,32 @@
 package com.amazon.connector.s3;
 
-/** Represents APIs of an Amazon S3 compatible object store */
-public interface ObjectClient {}
+import software.amazon.awssdk.core.ResponseInputStream;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+import software.amazon.awssdk.services.s3.model.HeadObjectRequest;
+import software.amazon.awssdk.services.s3.model.HeadObjectResponse;
+
+/**
+ * Represents APIs of an Amazon S3 compatible object store
+ */
+public interface ObjectClient {
+
+
+  /**
+   * Make a headObject request to S3.
+   *
+   * @param headObjectRequest HeadObjectRequest
+   * @return HeadObjectResponse
+   */
+  HeadObjectResponse headObject(HeadObjectRequest headObjectRequest);
+
+  /**
+   * Make a getObject request to S3.
+   *
+   * @param getObjectRequest GetObjectRequest
+   * @return ResponseInputStream<GetObjectResponse>
+   */
+  ResponseInputStream<GetObjectResponse> getObject(GetObjectRequest getObjectRequest);
+
+}
+
