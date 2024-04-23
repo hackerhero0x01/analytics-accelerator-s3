5f17ead7f1ac71e64b3da0b00ca964f19614f971
Geza Csenger <gccsenge@amazon.com>
2024-04-23 11:29:11 +0100

    Add unit test

--- input-stream/src/test/java/com/amazon/connector/s3/util/S3URITest.java
+++ input-stream/src/test/java/com/amazon/connector/s3/util/S3URITest.java
@@ -1,5 +1,6 @@
 package com.amazon.connector.s3.util;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
@@ -10,5 +11,13 @@ public class S3URITest {
   void testCreateValidatesArguments() {
     assertThrows(NullPointerException.class, () -> S3URI.of(null, "key"));
     assertThrows(NullPointerException.class, () -> S3URI.of("bucket", null));
+  }
+
+  @Test
+  void testCreate() {
+    S3URI s3URI = S3URI.of("bucket", "key");
+
+    assertEquals("bucket", s3URI.getBucket());
+    assertEquals("key", s3URI.getKey());
   }
 }
