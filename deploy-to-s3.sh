fa834d20c870a6a40024c73ad058d7e9a8ac8f17
Geza Csenger <gccsenge@amazon.com>
2024-05-02 20:28:04 +0000

    add deploy script

--- deploy-to-s3.sh
+++ deploy-to-s3.sh
@@ -1,2 +1,3 @@
 #!/bin/zsh
-
+aws s3 cp /local/home/gccsenge/open-source/s3-connector-framework/object-client/build/libs/object-client.jar s3://yo-ho-ho
+aws s3 cp /local/home/gccsenge/open-source/s3-connector-framework/input-stream/build/libs/input-stream.jar s3://yo-ho-ho
