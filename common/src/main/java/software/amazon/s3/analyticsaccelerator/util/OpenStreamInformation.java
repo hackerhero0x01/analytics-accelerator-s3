0572b76a9c1c2821568e5faa62fd2dbd998cc3b0
vaibhav5140 <vaibhavahuja900@gmail.com>
2025-07-01 08:34:45 +0100

    Iostats:AAL

--- common/src/main/java/software/amazon/s3/analyticsaccelerator/util/OpenStreamInformation.java
+++ common/src/main/java/software/amazon/s3/analyticsaccelerator/util/OpenStreamInformation.java
@@ -42,6 +42,7 @@ public class OpenStreamInformation {
   private final StreamAuditContext streamAuditContext;
   private final ObjectMetadata objectMetadata;
   private final InputPolicy inputPolicy;
+  @Builder.Default private final RequestCallback requestCallback = RequestCallback.NO_OP;
   private final EncryptionSecrets encryptionSecrets;
 
   /** Default set of settings for {@link OpenStreamInformation} */
