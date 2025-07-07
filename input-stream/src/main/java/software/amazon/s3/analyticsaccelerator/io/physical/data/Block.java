a897d1f139fb4e5a1e79292d10d5aef5a5ee2a21
Fuat Basik <fuatbasik@gmail.com>
2025-07-07 13:27:59 +0100

    add missing metrics update line back

--- input-stream/src/main/java/software/amazon/s3/analyticsaccelerator/io/physical/data/Block.java
+++ input-stream/src/main/java/software/amazon/s3/analyticsaccelerator/io/physical/data/Block.java
@@ -122,6 +122,8 @@ public class Block implements Closeable {
             .referrer(referrer)
             .build();
 
+    openStreamInformation.getRequestCallback().onGetRequest();
+
     this.source =
         this.telemetry.measureCritical(
             () ->
@@ -132,9 +134,10 @@ public class Block implements Closeable {
                     .attribute(StreamAttributes.range(this.blockKey.getRange()))
                     .attribute(StreamAttributes.generation(generation))
                     .build(),
-            objectClient.getObject(getRequest, openStreamInformation));
-
-    openStreamInformation.getRequestCallback().onGetRequest();
+            () -> {
+              this.aggregatingMetrics.add(MetricKey.GET_REQUEST_COUNT, 1);
+              return objectClient.getObject(getRequest, openStreamInformation);
+            });
 
     // Handle IOExceptions when converting stream to byte array
     this.data =
