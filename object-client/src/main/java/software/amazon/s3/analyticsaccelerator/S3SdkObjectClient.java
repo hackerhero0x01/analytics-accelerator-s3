e25d04fb84a0f8a6be3af219152168e8b924622a
rajdchak <rajdchak@amazon.co.uk>
2025-07-11 12:37:19 +0100

    Minor change

--- object-client/src/main/java/software/amazon/s3/analyticsaccelerator/S3SdkObjectClient.java
+++ object-client/src/main/java/software/amazon/s3/analyticsaccelerator/S3SdkObjectClient.java
@@ -29,6 +29,7 @@ import java.util.function.Function;
 import lombok.Getter;
 import lombok.NonNull;
 import software.amazon.awssdk.awscore.AwsRequestOverrideConfiguration;
+import software.amazon.awssdk.core.SdkServiceClientConfiguration;
 import software.amazon.awssdk.core.async.AsyncResponseTransformer;
 import software.amazon.awssdk.core.client.config.SdkAdvancedClientOption;
 import software.amazon.awssdk.services.s3.S3AsyncClient;
@@ -104,16 +105,11 @@ public class S3SdkObjectClient implements ObjectClient {
         new ConfigurableTelemetry(objectClientConfiguration.getTelemetryConfiguration());
     this.userAgent = new UserAgent();
     this.userAgent.prepend(objectClientConfiguration.getUserAgentPrefix());
-    String customUserAgent = "";
-    if (s3AsyncClient.serviceClientConfiguration() != null
-        && s3AsyncClient.serviceClientConfiguration().overrideConfiguration() != null) {
-      customUserAgent =
-          s3AsyncClient
-              .serviceClientConfiguration()
-              .overrideConfiguration()
-              .advancedOption(SdkAdvancedClientOption.USER_AGENT_PREFIX)
-              .orElse("");
-    }
+    String customUserAgent =
+        Optional.ofNullable(s3AsyncClient.serviceClientConfiguration())
+            .map(SdkServiceClientConfiguration::overrideConfiguration)
+            .flatMap(override -> override.advancedOption(SdkAdvancedClientOption.USER_AGENT_PREFIX))
+            .orElse("");
     this.userAgent.prepend(customUserAgent);
   }
 
